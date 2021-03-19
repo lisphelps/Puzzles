7b6fdefe899649b65cc6b2c17ab6b00c548ced01
Lis Phelps <79932328+lisphelps@users.noreply.github.com>
2021-03-19 08:28:50 -0500

    Coding Puzzles File Creation

---
+++ CodingPuzzles.js
@@ -0,0 +1,73 @@
+/* arrayCount9: Given an array of ints, 
+return the number of 9's in the array. 
+([1, 2, 9] → 1, [1, 9, 9] → 2, [1, 9, 9, 3, 9] → 3) */
+
+
+
+/* gHappy: We'll say that a lowercase 'g' in a string is "happy" 
+if there is another 'g' immediately to its left or right. 
+Return true if all the g's in the given string are happy. 
+("xxggxx" → true, "xxgxx" → false, "xxggyygxx" → false) */
+
+
+
+/* loneSum: Given 3 int values, a b c, return their sum. 
+However, if one of the values is the same as another of the values, 
+it does not count towards the sum. 
+([1, 2, 3] → 6, [3, 2, 3] → 2, [3, 3, 3] → 0) */
+
+
+
+/* seriesUp: Given n>=0, create an array with the pattern 
+{1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n} (spaces added to show the grouping). 
+Note that the length of the array will be 1 + 2 + 3 ... + n, 
+which is known to sum to exactly n*(n + 1)/2. 
+(3 → [1, 1, 2, 1, 2, 3], 4 → [1, 1, 2, 1, 2, 3, 1, 2, 3, 4], 2 → [1, 1, 2]) */
+
+
+
+/* doubling: Given a list of integers, 
+return a list where each integer is multiplied by 2. 
+([1, 2, 3] → [2, 4, 6], [6, 8, 6, 8, -1] → [12, 16, 12, 16, -2], [] → []) */
+
+
+
+/* zeroFront: Return an array that contains the exact same numbers as the given array, 
+but rearranged so that all the zeros are grouped at the start of the array. 
+The order of the non-zero numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. 
+You may modify and return the given array or make a new array. 
+([1, 0, 0, 1] → [0, 0, 1, 1], [0, 1, 1, 0, 1] → [0, 0, 1, 1, 1], [1, 0] → [0, 1]) */
+
+
+
+/* wordMultiple: Given an array of strings, return a Map<String, 
+Boolean> where each different string is a key and its value is true if that string appears 2 or more times in the array. 
+(["a", "b", "a", "c", "b"] → {"a": true, "b": true, "c": false}, 
+["c", "b", "a"] → {"a": false, "b": false, "c": false}, ["c", "c", "c", "c"] → {"c": true} */
+
+
+
+/* evenlySpaced: Given three ints, a b c, one of them is small, one is medium and one is large. 
+Return true if the three values are evenly spaced, 
+so the difference between small and medium is the same as the difference between medium and large. 
+([2, 4, 6] → true, [4, 6, 2] → true, [4, 6, 3] → false) */
+
+
+
+/* mirrorEnds: Given a string, look for a mirror image (backwards) string 
+at both the beginning and end of the given string. 
+In other words, zero or more characters at the very begining of the given string, 
+and at the very end of the string in reverse order (possibly overlapping). 
+For example, the string "abXYZba" has the mirror end "ab". 
+("abXYZba" → "ab", "abca" → "a, "aba" → "aba") */
+
+
+
+/* notReplace: Given a string, return a string where 
+every appearance of the lowercase word "is" has been replaced with "is not". 
+The word "is" should not be immediately preceeded or followed by a letter -- 
+so for example the "is" in "this" does not count. 
+("is test" → "is not test", "is-is" → "is not-is not", "This is right" → "This is not right") */
+
+
+
